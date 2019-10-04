f2136d5526e38bca0ac443665871c5a150e9af63
PawanBishnoi9383 <49333546+PawanBishnoi9383@users.noreply.github.com>
2019-10-04 14:46:47 +0530

    Update FixNegativeAmount.js
    ok

--- FixNegativeAmount.js
+++ FixNegativeAmount.js
@@ -1,7 +1,8 @@
 function fixNegative(amount){
+    amount = parseInt(amount);
     if( amount < 0){
         return -(amount); 
     }
-    return amount;
+    return Number(amount);
 }
 module.exports = fixNegative;
