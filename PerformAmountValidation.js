70edeab97b50b0124e5a6f24301b6707d5ecd1b6
PawanBishnoi9383 <49333546+PawanBishnoi9383@users.noreply.github.com>
2019-10-04 15:32:14 +0530

    Update PerformAmountValidation.js
    ok

--- PerformAmountValidation.js
+++ PerformAmountValidation.js
@@ -1,12 +1,12 @@
 const FixNegative = require('./FixNegativeAmount');
 function validateAmount(amount){
-    var amt = amount.toString();
+    var amt = parseInt(amount).toString();
    try{
     if(isNaN(amount)){
         throw "Amount Must Be In Digits Only";
     }
-    if(amt.length > 11){
-        throw "Only Amount Less Then 99 Arab Can Converted";
+    if(amt.length > 13){
+        throw "Only Amount Less Then 99 Kharab Can Converted";
     }
    }catch(err){
       console.log(err)
