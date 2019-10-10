603e7a567cc87f2e9d7919940dde5a5de26c63fd
sumil <sumilprincejain@gmail.com>
2019-10-11 04:13:54 +0530

    giving more access via options

--- numberFormatter/Number.js
+++ numberFormatter/Number.js
@@ -1,23 +1,31 @@
-let numberString = '', numWords = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
-  x = 0;
+let numWords = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
 
-function startProcess(number) {
-  if (typeof number == "number" && number.toString().length > 15)
-    throw new Error("Use string to go beyond 15 digits !");
-
-  if (isNaN(number)) return "NaN";
-
-  if (number < 0) {
-    number = -(number);
-  }
-  if (!Number.isInteger(Number(number))) number = parseInt(number);
-
-  number = number.toString().split('');
-  for (x = 0; x < number.length - 1; x++)
-    numberString += numWords[number[x]] + " ";
-  numberString += numWords[number[x]];
-
+function startProcess(originalNumber, options = {}) {
+  let numberString, number = originalNumber.toString();
+  if (typeof originalNumber == "number" && number.length > 15)
+    throw new Error("Need string to go beyond 15 digits!");
+  if (isNaN(originalNumber)) return "NaN";
+  number = number.replace('-','').split('.');
+  numberString = convertToWords(number[0]);
+  if (options.decimalValue && !isNaN(number[1]))
+    numberString += ` ${options.decimalValue} ${convertToWords(number[1])}`;
+  if (Number(originalNumber) < 0 && options.signValue)
+    numberString = `${options.signValue} ${numberString}`;
+  if (options.prefix)
+    numberString = `${options.prefix} ${numberString}`;
+  if (options.suffix)
+    numberString += ` ${options.suffix}`;
   return numberString;
 }
 
 module.exports = startProcess;
+
+
+function convertToWords(number) {
+  let numberString = '', x;
+  number = number.split('');
+  for (x = 0; x < number.length - 1; x++)
+    numberString += numWords[number[x]] + " ";
+  numberString += numWords[number[x]];
+  return numberString;
+}
