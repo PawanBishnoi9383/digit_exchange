f8220615edaba93d4db9359fc9730dbcfd67308e
sumil <sumilprincejain@gmail.com>
2019-10-16 01:44:29 +0530

    making compatible to lower version

--- numberFormatter/Number.js
+++ numberFormatter/Number.js
@@ -12,35 +12,35 @@ let numWords = [
 ];
 
 function startProcess(originalNumber, options = {}) {
-  let numberString, number = Number(originalNumber).toString();
+  var numberString, number = Number(originalNumber).toString();
   if (typeof originalNumber == "number" && number.length > 15)
     throw new Error("Need string to go beyond 15 digits!");
   if (isNaN(originalNumber)) return "NaN";
   number = number.replace('-', '').split('.');
   numberString = convertToWords(number[0], options);
   if (options.decimalValue && !isNaN(number[1]))
-    numberString += ` ${options.decimalValue} ${convertToWords(number[1], options)}`;
+    numberString += ' ' + options.decimalValue + ' ' + convertToWords(number[1], options);
   if (Number(originalNumber) < 0 && options.signValue)
-    numberString = `${options.signValue} ${numberString}`;
+    numberString = options.signValue + ' ' + numberString;
   if (options.prefix)
-    numberString = `${options.prefix} ${numberString}`;
+    numberString = options.prefix + ' ' + numberString;
   if (options.suffix)
-    numberString += ` ${options.suffix}`;
+    numberString += ' ' + options.suffix;
   return numberString;
 }
 
 module.exports = startProcess;
 
 
-function convertToWords(number, {letterCase = "title"}) {
-  let numberString = '', x;
+function convertToWords(number, ref) {
+  var letterCase = ref.letterCase ? ref.letterCase : "title" ;
+  var numberString = '', x;
   number = number.split('');
   letterCase = letterCase.toLowerCase();
   if (letterCase === "sentence") {
     numberString += numWords[number[0]]['title'] + " ";
     letterCase = "lower";
-  } else
-    numberString += numWords[number[0]][letterCase] + " ";
+  } else numberString += numWords[number[0]][letterCase] + " ";
   if (number.length > 1) {
     for (x = 1; x < number.length - 1; x++)
       numberString += numWords[number[x]][letterCase] + " ";
